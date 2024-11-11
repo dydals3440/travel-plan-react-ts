c47532c20fbcb9f8b09088b46579c2c601309541
김용민 <dydals3440@gmail.com>
2024-11-12 02:45:49 +0900

    [9-7]: Home API 연동

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,52 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import CityList from '../../components/home/CityList';
 import FilterList from '../../components/home/FilterList';
 import SearchInput from '../../components/home/SearchInput';
-import { City } from '../../types';
 import NarrowLayout from '@/components/common/NarrowLayout';
+import { getCities, getSearchedCities } from '@/services/home';
+import Loading from '@/components/common/Loading';
+import { useState } from 'react';
 
 export default function Home() {
-  //   const { data } =
-  //     useQuery();
-  //     /* 국가 필터, 검색 필터를 같이 넘겨 */
-  return (
+  const [q, setQ] = useState('');
+
+  const { isLoading, data } = useQuery({
+    queryKey: ['cities', q],
+    queryFn: q ? () => getSearchedCities(q) : getCities,
+  });
+
+  return isLoading || !data ? (
+    <Loading />
+  ) : (
     <NarrowLayout className='flex flex-col items-center my-30'>
       {/* 검색창 */}
       <div className='w-[339px] mb-24'>
-        <SearchInput onCompositionEnd={(value) => console.log(value)} />
+        <SearchInput onCompositionEnd={(value) => setQ(value)} />
       </div>
       {/* 국가 필터 */}
       {/* 여행지 리스트 */}
       <div className='mb-21'>
         <FilterList active='all' onChange={() => {}} />
       </div>
-      <CityList cities={DUMMY_DATA} />
+      <CityList cities={data} />
     </NarrowLayout>
   );
 }
-
-const DUMMY_DATA: City[] = [
-  {
-    city: 'seoul',
-    name: 'Seoul',
-    description: 'The capital of South Korea',
-    thumbnail: 'https://picsum.photos/300/200?random=1',
-  },
-  {
-    city: 'tokyo',
-    name: 'Tokyo',
-    description: 'The capital of Japan',
-    thumbnail: 'https://picsum.photos/300/200?random=2',
-  },
-  {
-    city: 'paris',
-    name: 'Paris',
-    description: 'The capital of France',
-    thumbnail: 'https://picsum.photos/300/200?random=3',
-  },
-  {
-    city: 'paris2',
-    name: 'Paris2',
-    description: 'The capital of France',
-    thumbnail: 'https://picsum.photos/300/200?random=5',
-  },
-];
