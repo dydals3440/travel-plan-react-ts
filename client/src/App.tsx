c05c728ae1104ddebe3aace01554cd9112d5db16
김용민 <dydals3440@gmail.com>
2024-11-11 18:52:15 +0900

    [9-3] feat: Routing추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import RegisterCity from './components/admin/RegisterCity/RegisterCity';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Loading from './components/common/Loading';
+
+// 네트워크 트래픽과 리소스 사용을 줄일 수 있음.
+const RegisterCity = lazy(
+  () => import('./pages/admin/RegisterCity/RegisterCity')
+);
 
 function App() {
   return (
-    <>
-      <RegisterCity />
-    </>
+    <BrowserRouter>
+      {/* 라우트 그룹을 관리하는 Routes */}
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path='admin'>
+            <Route path='register-city' element={<RegisterCity />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
