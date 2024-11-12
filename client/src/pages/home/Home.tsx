b02d9835cfe3044cc190326c51cdfb3aeea5cbf3
김용민 <dydals3440@gmail.com>
2024-11-12 16:28:25 +0900

    [9-8] Modal 컴포넌트 추가

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -6,8 +6,6 @@ import NarrowLayout from '@/components/common/NarrowLayout';
 import { getCities, getSearchedCities } from '@/services/home';
 import Loading from '@/components/common/Loading';
 import { useState } from 'react';
-import { useModalStore } from '@/store';
-import Modal, { ModalBackdrop, ModalPanel } from '@/components/common/Modal';
 
 export default function Home() {
   const [q, setQ] = useState('');
@@ -17,26 +15,10 @@ export default function Home() {
     queryFn: q ? () => getSearchedCities(q) : getCities,
   });
 
-  const { openModal } = useModalStore();
-  // modal
-  const handleClick = () => {
-    openModal(({ onClose }) => (
-      <Modal>
-        <ModalBackdrop />
-        <ModalPanel>
-          <div className='bg-[#ffffff]'>
-            <button onClick={onClose}>닫기</button>
-          </div>
-        </ModalPanel>
-      </Modal>
-    ));
-  };
-
   return isLoading || !data ? (
     <Loading />
   ) : (
     <>
-      <button onClick={handleClick}>모달 열기</button>
       <NarrowLayout className='flex flex-col items-center my-30'>
         {/* 검색창 */}
         <div className='w-[339px] mb-24'>
